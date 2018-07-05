3bfecb535655c96e07176acb66f6d77b8d7d6b05
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2018-07-05 23:17:55 +0200

    average via pandas

--- main.py
+++ main.py
@@ -5,27 +5,30 @@
 """Student Gradebook Application – Keep track of students (with a student class that has their name, average, and scores) in a class and their grades. Assign their scores on tests and assignments to the students and figure out their average and grade for the class. For added complexity put the students on a bell curve."""
 
 import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statistics
 
 random.seed(9)
 studentCount = 100
 
-class Grade():
-	def __init__(self,gradeNum):
-		self.students = []
-		self.gradeNum = gradeNum
-		self.size = len(self.students)
+class School():
+	def __init__(self):
+		self.students = pd.DataFrame(data=[],columns=['Grade','Grade Average'])
+		self.size = self.students.shape[0] #row count
 		return
 
 	def addStudent(self,student):
-		self.students.append(student)
-		self.size += 1
+		self.students = self.students.append(pd.Series([student.grade,student.average],index=self.students.columns.tolist(),name=student.name))
+		self.size = self.students.shape[0] #row count
 		return
 
 	def calcAverage(self):
-		if len(self.students) > 0:
-			self.average = round(sum([x.average for x in self.students])/len(self.students),2)
-			self.valedictorian = tuple([(x.name,x.average) for x in self.students if x.average == max([y.average for y in self.students])])
-			self.classclown = tuple([(x.name,x.average) for x in self.students if x.average == min([y.average for y in self.students])])
+		if self.size > 0:
+			self.average = self.students.groupby(['Grade']).agg({'Grade Average':statistics.mean})
+#			self.valedictorian = tuple([(x.name,x.average) for x in self.students if x.average == max([y.average for y in self.students])]) #improve this
+#			self.classclown = tuple([(x.name,x.average) for x in self.students if x.average == min([y.average for y in self.students])]) #and this
 		else:
 			self.average = 0
 			self.valedictorian = 0
@@ -44,9 +47,10 @@ class Grade():
 		return
 
 class Student():
-	def __init__(self,name,age):
+	def __init__(self,name,age,grade):
 		self.name = name
 		self.age = age
+		self.grade = grade
 		self.scores = []
 		self.average = 0
 		self.updateAverage()
@@ -69,20 +73,19 @@ studentNames = ['Sally','Joe','Bill','Pete','Aaron','Diana','Camilla']
 lastNames = ['Bo','West','Cook','Bauer','Rogan','Stu','Oak']
 gradeWeights = [random.randrange(1,5) for x in range(0,10)]
 
-grade1 = Grade(1)
+grade1 = School()
 
 for i in range(0,studentCount):
-	students[i] = Student(random.choice(studentNames)+" "+random.choice(lastNames),13)
+	students[i] = Student(random.choice(studentNames)+" "+random.choice(lastNames),13,1)
 	for j in range(0,10):
 		students[i].addGrade(random.randrange(0,101),gradeWeights[j])
 	grade1.addStudent(students[i])
 
-printJob = sorted([(fulano.name,str(fulano.average)) for fulano in students.values()],key=lambda x:x[1],reverse=True)
-for item in printJob:
-	print(item[0]+",\t",item[1])
+grade1.students.sort_values(by='Grade Average',inplace=True,ascending=False)
 grade1.calcAverage()
-grade1.calcMedian()
-print("\nClass average: %s"%grade1.average)
-print("\nClass median: %s"%grade1.median)
-print("\nValedictorian: %s"%grade1.valedictorian)
-print("\nClass clown: %s"%grade1.classclown)
+#grade1.calcMedian()
+print(grade1.students)
+print("\nClass average: ",grade1.average)
+#print("\nClass median: %s"%grade1.median)
+#print("\nValedictorian: %s"%grade1.valedictorian)
+#print("\nClass clown: %s"%grade1.classclown)
